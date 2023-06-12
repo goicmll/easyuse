5e3d20e1b513a37dc762a5b2274b4482dfd5fd83
icmll <icmll@qq.com>
2023-06-12 13:52:57 +0800

    Update random.go

--- random.go
+++ random.go
@@ -5,13 +5,16 @@ import (
 	"time"
 )
 
-func GenerateRandStr(n int) string {
-	var lettersLen = 69
-	var letters = Str2SliceByte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#%^&()")
+var L1 = Str2SliceByte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#%^&()")
+var L2 = Str2SliceByte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+var L3 = Str2SliceByte("0123456789")
+
+func GenerateRandStr(n int, source []byte) string {
+	var lettersLen = len(source)
 	rd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	r := make([]byte, n)
 	for i := range r {
-		r[i] = letters[rd.Intn(lettersLen)]
+		r[i] = source[rd.Intn(lettersLen)]
 	}
 	return Bytes2Str(r)
 }
