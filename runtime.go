0500d2bdc22d0c3a703f7adb22d22e76d4d63c7f
icmll <icmll@qq.com>
2023-06-28 21:17:16 +0800

    优化

--- runtime.go
+++ runtime.go
@@ -2,9 +2,8 @@ package easyuse
 
 import "runtime"
 
-
 func GetCurrentGoroutineStack(bufByte int64) string {
 	var buf = make([]byte, 0, bufByte)
 	n := runtime.Stack(buf, false)
-	return string(buf[:n])
+	return Bytes2Str(buf[:n])
 }
