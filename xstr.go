c8322e871d90be268d9d79301fd705cabf22d071
icmll <icmll@qq.com>
2023-07-18 16:43:04 +0800

    Update xstr.go

--- xstr.go
+++ xstr.go
@@ -3,6 +3,8 @@ package easyuse
 import (
 	"math/rand"
 	"time"
+	"reflect"
+	"unsafe"
 )
 
 // 预定义随机字符串源
@@ -21,4 +23,30 @@ func Random(n int, source []byte) string {
 		r[i] = source[rd.Intn(lettersLen)]
 	}
 	return Bytes2Str(r)
+}
+
+// string to []byte
+func Str2SliceByte(s string) []byte {
+	var b []byte
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
+}
+
+// bytes to string
+func Bytes2Str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// 生成占位符
+//  Placeholder(5, "?")  return: "?,?,?,?,?"
+func Placeholder(n int, holder string) string {
+	ph := make([]string, n)
+	for i := 0; i < n; i++ {
+		ph[i] = holder
+	}
+	return strings.Join(ph, ",")
 }
