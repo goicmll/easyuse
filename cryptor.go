717017c9dee922192e8d0a9dccbb45a1b3c5874c
icmll <icmll@qq.com>
2023-09-04 22:09:44 +0800

    优化结构

--- cryptor.go
+++ cryptor.go
@@ -4,9 +4,18 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/md5"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"io"
+	"time"
 )
 
 // AesCBCStrEncrypt key长度必须为16, 24或者32, 返回hex.EncodeToString 后的字符串
@@ -149,4 +158,114 @@ func pkcs7UnPadding(origin []byte) ([]byte, error) {
 		return Str2SliceByte(""), NewEasyUseError("解密失败!")
 	}
 	return origin[:(length - unpadding)], nil
+}
+
+// BcryptStr 字符串生成bcrypt
+func BcryptStr(str string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword(Str2SliceByte(str), bcrypt.DefaultCost)
+	if err != nil {
+		return "", NewEasyUseError(err.Error())
+	}
+	return Bytes2Str(b), nil
+}
+
+// BcryptCompare 比较密文和字符串
+func BcryptCompare(strHash, str string) bool {
+	byteHash := Str2SliceByte(strHash)
+	bytePwd := Str2SliceByte(str)
+	err := bcrypt.CompareHashAndPassword(byteHash, bytePwd)
+	return err == nil
+}
+
+// JwtMake 制作jwt串
+func JwtMake(id, secret, issuer, subject, audience string, maxAge int) (string, error) {
+	claim := jwt.RegisteredClaims{
+		Issuer:    issuer,
+		Subject:   subject,
+		Audience:  []string{audience},
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(maxAge) * time.Second)),
+		NotBefore: jwt.NewNumericDate(time.Now()),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ID:        id,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	jwtStr, err := token.SignedString(Str2SliceByte(secret))
+	if err != nil {
+		return "", NewEasyUseError(err.Error())
+	}
+	return jwtStr, nil
+}
+
+// JwtParse 解析jwt
+func JwtParse(jwtStr, secret string) (*jwt.RegisteredClaims, error) {
+	tok, err := jwt.ParseWithClaims(jwtStr, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return Str2SliceByte(secret), nil
+	})
+	if err != nil {
+		return nil, NewEasyUseError("token 不可用!")
+	}
+	if claims, ok := tok.Claims.(*jwt.RegisteredClaims); ok && tok.Valid {
+		return claims, nil
+	}
+	return nil, NewEasyUseError("token 不可用!")
+}
+
+// RsaDecrypt 解密
+func RsaDecrypt(originBase64, privateKey string) (string, error) {
+	originByte, err := base64.StdEncoding.DecodeString(originBase64)
+	if err != nil {
+		return "", NewEasyUseError("密文base64解码错误！")
+	}
+	keyByte := []byte(privateKey)
+	//解密
+	block, _ := pem.Decode(keyByte)
+	if block == nil {
+		return "", NewEasyUseError("RSA 私钥解码错误！")
+	}
+	//解析PKCS1格式的私钥
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return "", NewEasyUseError("RSA 解析私钥错误！")
+	}
+	// 解密
+	out, err := rsa.DecryptPKCS1v15(rand.Reader, priv, originByte)
+	if err != nil {
+		return "", NewEasyUseError("RSA 密文解密错误")
+	}
+	return Bytes2Str(out), nil
+}
+
+// RsaEncrypt ########### RSA 加解密
+func RsaEncrypt(origin, publicKey string) (string, error) {
+	originByte := []byte(origin)
+	keyByte := []byte(publicKey)
+	//解密pem格式的公钥
+	block, _ := pem.Decode(keyByte)
+	if block == nil {
+		return "", NewEasyUseError("RSA 公钥解码错误！")
+	}
+	// 解析公钥
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return "", NewEasyUseError("RSA 解析公钥错误！")
+	}
+	// 类型断言
+	pub, ok := pubInterface.(*rsa.PublicKey)
+	if !ok {
+		return "", NewEasyUseError("RSA 解析公钥错误！")
+	}
+	//加密
+	out, err := rsa.EncryptPKCS1v15(rand.Reader, pub, originByte)
+	if err != nil {
+		return "", NewEasyUseError("RSA 密文加密错误！")
+	}
+	return base64.StdEncoding.EncodeToString(out), nil
+}
+
+// StringMd5 md5
+func StringMd5(s string) string {
+	data := Str2SliceByte(s) //切片
+	has := md5.Sum(data)
+	md5str := fmt.Sprintf("%x", has) //将[]byte转成16进制
+	return md5str
 }
